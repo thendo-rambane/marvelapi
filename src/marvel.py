1f50ac8416550478b71d540e484e8a1b3e3e5d5e
Thendo Rambane <rambane.t.r@gmail.com>
2019-07-07 17:51:45 +0200

    Fix bug:     *No return statements were given for search by multiple parameters

--- src/marvel.py
+++ src/marvel.py
@@ -1,7 +1,7 @@
 import json
 import os
 import os.path as path
-# import requests
+import requests
 from typing import overload
 
 from .authenticator import Authenticator as Auth
@@ -11,14 +11,9 @@ from .querier import Querier
 PUBLIC_KEY = '9b6d7545e698ec76a8673df57089edbc'
 PRIVATE_KEY = 'd55e975ae3c3a0e3041d7f8688e126c3f0d517dc'
 
-# auth = Auth(PUBLIC_KEY, PRIVATE_KEY)
 
 class Error(Exception):
     pass
-
-q_test_path = path.abspath(path.curdir)+'/tests/'
-q_test_data_file = q_test_path+'test_data/public_docs.json'
-public_documentation = json.load(open(q_test_data_file))
 
 
 class MarvelAPI():
@@ -50,46 +45,40 @@ class MarvelAPI():
         results = self.__run_endpoint_request(query)
         if results:
             return [Entity(entity, result) for result in results]
-        else:
-            return None
 
     def character_by_id(self, id: int):
-        self.__get_by_id('characters', id)
+        return self.__get_by_id('characters', id)
 
-    def characters(self, **kwargs):
-        self.__get('characters', **kwargs)
+    def get_characters_where(self, **kwargs):
+        return self.__get('characters', **kwargs)
 
     def creator_by_id(self, id: int):
-        self.__get_by_id('creators', id)
+        return self.__get_by_id('creators', id)
 
-    def creators(self, **kwargs):
-        self.__get('creators', **kwargs)
+    def get_creator_where(self, **kwargs):
+        return self.__get('creators', **kwargs)
 
     def comic_by_id(self, id: int):
-        self.__get_by_id('comics', id)
+        return self.__get_by_id('comics', id)
 
-    def comics(self, **kwargs):
-        self.__get('comics', **kwargs)
+    def get_comic_where(self, **kwargs):
+        return self.__get('comics', **kwargs)
 
     def event_by_id(self, id: int):
-        self.__get_by_id('events', id)
+        return self.__get_by_id('events', id)
 
-    def events(self, **kwargs):
-        self.__get('events', **kwargs)
+    def get_event_where(self, **kwargs):
+        return self.__get('events', **kwargs)
 
     def series_by_id(self, id: int):
-        self.__get_by_id('series', id)
+        return self.__get_by_id('series', id)
 
-    def series(self, **kwargs):
-        self.__get('series', **kwargs)
+    def get_series_where(self, **kwargs):
+        return self.__get('series', **kwargs)
 
     def story_by_id(self, id: int):
-        self.__get_by_id('stories', id)
+        return self.__get_by_id('stories', id)
 
-    def stories(self, **kwargs):
-        self.__get('stories', **kwargs)
+    def get_stories_where(self, **kwargs):
+        return self.__get('stories', **kwargs)
 
-
-# if __name__ == "__main__":
-#     m = MarvelAPI(PUBLIC_KEY, PRIVATE_KEY)
-#     m.characters(name='james', nameStartsWith='ll')
