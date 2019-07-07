d30cc58708c230b7415a04c7658254057222ab93
Thendo Rambane <rambane.t.r@gmail.com>
2019-07-07 17:50:36 +0200

    Finish implementation     *Hide private members     *Implement requests for endpoint with exceptions

--- src/marvel.py
+++ src/marvel.py
@@ -13,14 +13,8 @@ PRIVATE_KEY = 'd55e975ae3c3a0e3041d7f8688e126c3f0d517dc'
 
 # auth = Auth(PUBLIC_KEY, PRIVATE_KEY)
 
-test_path = path.abspath(path.curdir)+'/tests/'
-test_data_files = os.listdir(path=test_path+'test_data/')
-test_json = {}
-for test_file in test_data_files:
-    if test_file.find('marvel') > -1:
-        key = test_file.split('.')[0].split('-')[1]
-        test_json[key] = json.load(open(test_path+'/test_data/'+test_file))
-results = test_json['character']['data']['results']
+class Error(Exception):
+    pass
 
 q_test_path = path.abspath(path.curdir)+'/tests/'
 q_test_data_file = q_test_path+'test_data/public_docs.json'
@@ -31,29 +25,28 @@ class MarvelAPI():
     """docstring for MA"""
 
     def __init__(self, public_key: str, private_key: str):
-        self.auth = Auth(public_key, private_key)
-        self.querier = None
+        self.__auth = Auth(public_key, private_key)
+        self.__querier = None
 
     def __run_endpoint_request(self, query: str):
-        # request = requests.get(query)
-        # if req.status_code != 200:
-        # 	print('ERROR')
-        # response = request.json()
-        # return response['data']['results']
-        return []
+        request = requests.get(query)
+        if request.status_code != 200:
+            error = request.json()['status']
+            raise Error(error)
+        response = request.json()
+        return response['data']['results']
 
     def __get_by_id(self, entity: str, id: int):
-        self.querier = Querier(entity, self.auth)
-        query = self.querier.get_query_string(id)
+        self.__querier = Querier(entity, self.__auth)
+        query = self.__querier.get_query_string(id)
         result = self.__run_endpoint_request(query)
-        return Entity(entity, result)
+        return Entity(entity, result[0])
 
     def __get(self, entity: str, **kwargs) -> list:
         """Key word argument overload"""
-        self.querier = Querier(entity, self.auth)
-        self.querier.load_values(public_documentation)
-        self.querier.add_parameters(**kwargs)
-        query = self.querier.get_query_string()
+        self.__querier = Querier(entity, self.__auth)
+        self.__querier.add_parameters(**kwargs)
+        query = self.__querier.get_query_string()
         results = self.__run_endpoint_request(query)
         if results:
             return [Entity(entity, result) for result in results]
